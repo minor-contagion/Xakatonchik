270f038f453ec6f30a431a3de118188bcdfe37ca
minor_contagion <arxeolog41@mail.ru>
2023-06-02 02:05:35 +0300

    12345

--- js/index.js
+++ js/index.js
@@ -1,59 +1,129 @@
-var cal = {
-    // (A) PROPERTIES
-    // (A1) FLAGS & DATA
-    sMon : false, // week start on monday
-    data : null, // events for selected period
-    sDay : 0, sMth : 0, sYear : 0, // selected day month year
-   
-    // (A2) MONTHS & DAY NAMES
-    months : [
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ],
-    days : ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"],
-   
-    // (A3) HTML ELEMENTS
-    hMth : null, hYear : null, // month/year selector
-    hWrap : null, // calendar wrapper
-    hFormWrap : null, hForm : null, // event form
-    hfDate : null, hfTxt : null, hfDel : null, // form fields
-   
-    // (B) INIT CALENDAR
-    init : () => {
-      // (B1) GET HTML ELEMENTS
-      cal.hMth = document.getElementById("calMonth");
-      cal.hYear = document.getElementById("calYear");
-      cal.hWrap = document.getElementById("calWrap");
-      cal.hFormWrap = document.getElementById("calForm");
-      cal.hForm = cal.hFormWrap.querySelector("form");
-      cal.hfDate = document.getElementById("evtDate");
-      cal.hfTxt = document.getElementById("evtTxt");
-      cal.hfDel = document.getElementById("evtDel");
-   
-      // (B2) APPEND MONTHS/YEAR
-      let now = new Date(), nowMth = now.getMonth();
-      cal.hYear.value = parseInt(now.getFullYear());
-      for (let i=0; i<12; i++) {
-        let opt = document.createElement("option");
-        opt.value = i;
-        opt.innerHTML = cal.months[i];
-        if (i==nowMth) { opt.selected = true; }
-        cal.hMth.appendChild(opt);
+var Cal = function(divId) {
+  //Сохраняем идентификатор div
+  this.divId = divId;
+  // Дни недели с понедельника
+  this.DaysOfWeek = [
+    'Пн',
+    'Вт',
+    'Ср',
+    'Чтв',
+    'Птн',
+    'Суб',
+    'Вск'
+  ];
+  // Месяцы начиная с января
+  this.Months =['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+  //Устанавливаем текущий месяц, год
+  var d = new Date();
+  this.currMonth = d.getMonth();
+  this.currYear = d.getFullYear();
+  this.currDay = d.getDate();
+};
+// Переход к следующему месяцу
+Cal.prototype.nextMonth = function() {
+  if ( this.currMonth == 11 ) {
+    this.currMonth = 0;
+    this.currYear = this.currYear + 1;
+  }
+  else {
+    this.currMonth = this.currMonth + 1;
+  }
+  this.showcurr();
+};
+// Переход к предыдущему месяцу
+Cal.prototype.previousMonth = function() {
+  if ( this.currMonth == 0 ) {
+    this.currMonth = 11;
+    this.currYear = this.currYear - 1;
+  }
+  else {
+    this.currMonth = this.currMonth - 1;
+  }
+  this.showcurr();
+};
+// Показать текущий месяц
+Cal.prototype.showcurr = function() {
+  this.showMonth(this.currYear, this.currMonth);
+};
+// Показать месяц (год, месяц)
+Cal.prototype.showMonth = function(y, m) {
+  var d = new Date()
+  // Первый день недели в выбранном месяце 
+  , firstDayOfMonth = new Date(y, m, 7).getDay()
+  // Последний день выбранного месяца
+  , lastDateOfMonth =  new Date(y, m+1, 0).getDate()
+  // Последний день предыдущего месяца
+  , lastDayOfLastMonth = m == 0 ? new Date(y-1, 11, 0).getDate() : new Date(y, m, 0).getDate();
+  var html = '<table>';
+  // Запись выбранного месяца и года
+  html += '<thead><tr>';
+  html += '<td colspan="7">' + this.Months[m] + ' ' + y + '</td>';
+  html += '</tr></thead>';
+  // заголовок дней недели
+  html += '<tr class="days">';
+  for(var i=0; i < this.DaysOfWeek.length;i++) {
+    html += '<td>' + this.DaysOfWeek[i] + '</td>';
+  }
+  html += '</tr>';
+  // Записываем дни
+  var i=1;
+  do {
+    var dow = new Date(y, m, i).getDay();
+    // Начать новую строку в понедельник
+    if ( dow == 1 ) {
+      html += '<tr>';
+    }
+    // Если первый день недели не понедельник показать последние дни предыдущего месяца
+    else if ( i == 1 ) {
+      html += '<tr>';
+      var k = lastDayOfLastMonth - firstDayOfMonth+1;
+      for(var j=0; j < firstDayOfMonth; j++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
       }
-   
-      // (B3) ATTACH CONTROLS
-      cal.hMth.onchange = cal.draw;
-      cal.hYear.onchange = cal.draw;
-      document.getElementById("calBack").onclick = () => cal.pshift();
-      document.getElementById("calNext").onclick = () => cal.pshift(1);
-      cal.hForm.onsubmit = cal.save;
-      document.getElementById("evtClose").onclick = () => cal.hFormWrap.close();
-      cal.hfDel.onclick = cal.del;
-   
-      // (B4) START - DRAW CALENDAR
-      if (cal.sMon) { cal.days.push(cal.days.shift()); }
-      cal.draw();
-    },
-    // ...
+    }
+    // Записываем текущий день в цикл
+    var chk = new Date();
+    var chkY = chk.getFullYear();
+    var chkM = chk.getMonth();
+    if (chkY == this.currYear && chkM == this.currMonth && i == this.currDay) {
+      html += '<td class="today">' + i + '</td>';
+    } else {
+      html += '<td class="normal">' + i + '</td>';
+    }
+    // закрыть строку в воскресенье
+    if ( dow == 0 ) {
+      html += '</tr>';
+    }
+    // Если последний день месяца не воскресенье, показать первые дни следующего месяца
+    else if ( i == lastDateOfMonth ) {
+      var k=1;
+      for(dow; dow < 7; dow++) {
+        html += '<td class="not-current">' + k + '</td>';
+        k++;
+      }
+    }
+    i++;
+  }while(i <= lastDateOfMonth);
+  // Конец таблицы
+  html += '</table>';
+  // Записываем HTML в div
+  document.getElementById(this.divId).innerHTML = html;
+};
+// При загрузке окна
+window.onload = function() {
+  // Начать календарь
+  var c = new Cal("divCal");			
+  c.showcurr();
+  // Привязываем кнопки «Следующий» и «Предыдущий»
+  getId('btnNext').onclick = function() {
+    c.nextMonth();
   };
-  window.onload = cal.init;
+  getId('btnPrev').onclick = function() {
+    c.previousMonth();
+  };
+}
+// Получить элемент по id
+function getId(id) {
+  return document.getElementById(id);
+}
